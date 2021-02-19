93693646f6a9e2ccfe95eebb4d72804ff0c7e8d8
Annnis <77993460+Annnis@users.noreply.github.com>
2021-02-19 18:57:22 +0200

    Update FunctionTest.java

--- src/com/company/FunctionTest.java
+++ src/com/company/FunctionTest.java
@@ -5,15 +5,55 @@ import org.junit.jupiter.api.Test;
 
 public class FunctionTest {
     @Test
-    public void findDate(){
+    public void findDate() {
         String actual = Function.dateByNum(5);
         String expected = "Friday";
-        Assert.assertEquals(actual,expected);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void distance() {
+        int actual = (int) Function.distance(5, 6, 8, 9);
+        int expected = 4;
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void getString5() {
+        String actual = Function.getStringByNumber(8);
+        String expected = "восемь  ";
+        Assert.assertEquals(actual, expected);
+
     }
     @Test
-    public void distance(){
-        int actual = (int) Function.distance(5,6,8,9);
-        int expected = 4;
-        Assert.assertEquals(actual,expected);
+    public void getString58() {
+        String actual = Function.getStringByNumber(58);
+        String expected = "пятьдесят восемь  ";
+        Assert.assertEquals(actual, expected);
+
+    }
+    @Test
+    public void getString581() {
+        String actual = Function.getStringByNumber(581);
+        String expected = "пятьсот восемьдесят один  ";
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void getNum5() {
+        int actual = Function.getNumberByString("пять");
+        int expected = 5;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void getNum15() {
+        int actual = Function.getNumberByString("пятнадцать");
+        int expected = 15;
+        Assert.assertEquals(actual, expected);
+    }
+    @Test
+    public void getNum151() {
+        int actual = Function.getNumberByString("сто пятьдесят один");
+        int expected = 151;
+        Assert.assertEquals(actual, expected);
     }
 }
